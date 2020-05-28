a7c7c484f78f29ddb7807718b4c1d9e93f99c008
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-29 00:32:21 +0500

    ss

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -6,6 +6,7 @@ from random import *
 # Initializing a variable. 
 characters = string.ascii_letters + string.punctuation + string.digits
 
+# Initializing a second variable. 
 password = "".join(choice(characters) for x in range(randint(8, 16)))
 
 print(password)
